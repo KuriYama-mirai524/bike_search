c20af28da7d0d53f53cb106d0ba76d5272fb5223
xiluo <xiluo@guimc.ltd>
2023-01-21 02:55:20 +0800

    优化代码质量

--- botlink.py
+++ botlink.py
@@ -1,25 +1,21 @@
-
 import requests
 
-def linkbot(key, port, botqq, host):
-    
-    # 连接到bot
-    host = 'http://'+str(host)+':'+str(port)
 
-    resp = requests.request('post', host+'/verify', json=key)
+def linkbot(key, port, botqq, host):
+    # 连接到bot
+    host = 'http://' + str(host) + ':' + str(port)
+
+    resp = requests.request('post', host + '/verify', json=key)
     print('获取会话...')
     if resp.json()['code'] == 400:
         print('会话获取失败,请检查端口或key')
     else:
         session = resp.json()['session']
         data = dict(sessionKey=resp.json()['session'], qq=botqq)
-        resp = requests.request('post', host+'/bind', json=data)
+        resp = requests.request('post', host + '/bind', json=data)
         print(resp.json())
         if resp.json()['code'] == 0:
             print('bot连接成功!')
         else:
             print('连接失败')
     return session, host
-
-
-
--- main.py
+++ main.py
@@ -1,141 +1,124 @@
-import botlink
-import simuse
 import time
-from selenium.webdriver import ChromeOptions
-import sougou_spider
-import messagesend
+
+import pandas as pd
 from selenium import webdriver
 from selenium.webdriver import ChromeOptions
-import time
-import pandas as pd
-import mengniang_spider
 
-excel = pd.read_excel('./config.xls', index_col=0, dtype='object')
+import botlink
+import moegirl_spider
+import simuse
+import sougou_spider
 
-group = excel['添加的群'].values
-adminqq = excel['管理员账号'].values  
-key = excel['Http KEY'][0]
-host = excel['HOST'][0]
-port = excel['PORT'][0]
-botqq = excel['BOT QQ'][0]
-code = excel['是否使用简略信息'][0]
-driver = excel['搜索引擎'][0]
-print('当前引擎:', driver)
-print('简略模式:', code)
-key = dict(verifyKey=key)
+CONFIG = pd.read_excel('./config.xls', index_col=0, dtype='object')
 
+GROUPS = CONFIG['添加的群'].values
+ADMIN_QQ = CONFIG['管理员账号'].values
+KEY = CONFIG['Http KEY'][0]
+HOST = CONFIG['HOST'][0]
+PORT = CONFIG['PORT'][0]
+BOT_QQ = CONFIG['BOT QQ'][0]
+DRIVER = CONFIG['搜索引擎'][0]
+
+if CONFIG['是否使用简略信息'][0] == "是":
+    LITE_MODE = True
+else:
+    LITE_MODE = False
+
+print('当前引擎:', DRIVER)
+print('简略模式:', LITE_MODE)
+KEY = dict(verifyKey=KEY)
 
 # 连接bot
-session, host = botlink.linkbot(key=key, host=host, port=port, botqq=botqq)
-print('启用该功能的群:', group)
+SESSION, _ = botlink.linkbot(key=KEY, host=HOST, port=PORT, botqq=BOT_QQ)
+print('启用该功能的群:', GROUPS)
 
 # 打开浏览器, 准备截图
-options = ChromeOptions()
-options.add_argument('--disable-software-rasterizer')
-options.add_argument('-ignore-certificate-errors')
-options.add_argument('--headless')
-options.add_argument('--disable-gpu')
-options.add_argument('--no-sandbox')
+OPTIONS = ChromeOptions()
+OPTIONS.add_argument('--disable-software-rasterizer')
+OPTIONS.add_argument('-ignore-certificate-errors')
+OPTIONS.add_argument('--headless')
+OPTIONS.add_argument('--disable-gpu')
+OPTIONS.add_argument('--no-sandbox')
 
-web = webdriver.Chrome(options=options)
-web.get('https://baike.sogou.com/')
+WEB_DRIVER = webdriver.Chrome(options=OPTIONS)
+WEB_DRIVER.get('https://baike.sogou.com/')
 
 # 开始监听会话
 while True:
-    msg = simuse.Fetch_Message(host=host, session=session, deal=1)
+    msg = simuse.fetch_message(host=HOST, session=SESSION, deal=1)
+
     if msg == 0:
         continue
     try:
-        g = msg[0]['group']
+        group = msg[0]['group']
         sender = msg[0]['sender']
         msg = msg[0]['messagechain'][1]['text']
-        
-        if str(g) not in str(group):
+
+        if str(group) not in str(GROUPS):
             continue
-        if '百科 ' in msg:
 
+        # 检查监听内容
+        if msg.startwith("百科 "):  # 搜索百科
             keyword = msg.replace('百科 ', '')
-            if driver == '搜狗百科':
-                if code == '是':
-                    messagesend.sendmessage_txt(group=g, session=session, message='简略模式搜索中....', host=host)
-                    img = sougou_spider.search_less(web=web, word=keyword)
-                    if img == 0:
-                        messagesend.sendmessage_txt(group=g, session=session, message='搜索出错，该词条未收录', host=host)
-                        continue
-                    print('发送图片中..')
-                    t1 = time.time()
-                    messagesend.sendmessage_img(group=g, session=session, message=img, host=host)
-                    t2 = time.time()
-                    print('发送用时', t2-t1,'s')
-                else:
-                    messagesend.sendmessage_txt(group=g, session=session, message='详细模式搜索中....等待时间可能较长', host=host)
-                    img = sougou_spider.search_more(web=web, word=keyword)
-                    if img == 0:
-                        messagesend.sendmessage_txt(group=g, session=session, message='搜索出错，该词条未收录', host=host)
-                        continue
-                    
-                    print('发送图片中..')
-                    t1 = time.time()
-                    messagesend.sendmessage_img(group=g, session=session, message=img, host=host)
-                    t2 = time.time()
-                    print('发送用时', t2-t1,'s')
-            if driver == '萌娘百科':
-                if code == '是':
-                    messagesend.sendmessage_txt(group=g, session=session, message='简略模式搜索中....', host=host)
-                    img = mengniang_spider.search_less(web=web, word=keyword)
-                    if img == 0:
-                        messagesend.sendmessage_txt(group=g, session=session, message='搜索出错，该词条未收录', host=host)
-                        continue
-                    print('发送图片中..')
-                    messagesend.sendmessage_img(group=g, session=session, message=img, host=host)
-                else:
-                    messagesend.sendmessage_txt(group=g, session=session, message='详细模式搜索中....等待时间可能较长', host=host)
-                    img = mengniang_spider.search_more(web=web, word=keyword)
-                    if img == 0:
-                        messagesend.sendmessage_txt(group=g, session=session, message='搜索出错，该词条未收录', host=host)
-                        continue
-                    print('发送图片中..')
-                    messagesend.sendmessage_img(group=g, session=session, message=img, host=host)
+            img = 0
+            if LITE_MODE == "是":
+                sender.send_str(group=group, session=SESSION, message='简略模式搜索中....', host=HOST)
 
-
-
-        
-        
-        elif '/切换模式' in msg:
-            if sender in adminqq:
-                if code == '是':
-                    code = '否'
-                    messagesend.sendmessage_txt(group=g, session=session, message='切换成功，当前为详细搜索', host=host)
+                # 获取图片
+                if DRIVER == "搜狗百科":
+                    img = sougou_spider.search_less(web=WEB_DRIVER, word=keyword)
+                elif DRIVER == "萌娘百科":
+                    img = moegirl_spider.search_less(web=WEB_DRIVER, word=keyword)
                 else:
-                    code = '是'
-                    messagesend.sendmessage_txt(group=g, session=session, message='切换成功，当前为简略搜索', host=host)
+                    raise Exception("WTF? 你填了什么?")
+            elif LITE_MODE == "否":
+                sender.send_str(group=group, session=SESSION, message='详细模式搜索中....等待时间可能较长',
+                                host=HOST)
+
+                # 获取图片
+                if DRIVER == "搜狗百科":
+                    img = sougou_spider.search_more(web=WEB_DRIVER, word=keyword)
+                elif DRIVER == "萌娘百科":
+                    img = moegirl_spider.search_more(web=WEB_DRIVER, word=keyword)
+                else:
+                    raise Exception("WTF? 你填了什么?")
+
+                # 检查图片
+                if img == 0:
+                    sender.send_str(group=group, session=SESSION, message='搜索出错，该词条未收录', host=HOST)
+                    continue
+            # 检查图片
+            if img == 0:
+                sender.send_str(group=group, session=SESSION, message='搜索出错，该词条未收录', host=HOST)
+                continue
+
+            print('Sending Image..')
+            t1 = time.time()
+            sender.send_image(group=group, session=SESSION, message=img, host=HOST)
+            print('Sent! [{}s]'.format(time.time() - t1))
+        elif '/切换模式' in msg:  # 更换搜索模式
+            if sender in ADMIN_QQ:
+                if LITE_MODE == '是':
+                    LITE_MODE = '否'
+                    sender.send_str(group=group, session=SESSION, message='切换成功，当前为详细搜索', host=HOST)
+                else:
+                    LITE_MODE = '是'
+                    sender.send_str(group=group, session=SESSION, message='切换成功，当前为简略搜索', host=HOST)
             else:
-                messagesend.sendmessage_txt(group=g, session=session, message='只有管理员可以切换模式', host=host)
+                sender.send_str(group=group, session=SESSION, message='只有管理员可以切换模式', host=HOST)
+        elif '/切换引擎' in msg:  # 更换搜索引擎
+            if sender in ADMIN_QQ:
+                if DRIVER == '搜狗百科':
+                    DRIVER = '萌娘百科'
+                    sender.send_str(group=group, session=SESSION, message='当前引擎切换为萌娘百科', host=HOST)
+                elif DRIVER == '萌娘百科':
+                    DRIVER = '搜狗百科'
+                    sender.send_str(group=group, session=SESSION, message='当前引擎切换为搜狗百科', host=HOST)
+                    WEB_DRIVER.get('https://baike.sogou.com/')
 
-        elif '/切换引擎' in msg:
-            if sender in adminqq:
-                if driver == '搜狗百科':
-                    driver = '萌娘百科'
-                    messagesend.sendmessage_txt(group=g, session=session, message='当前引擎切换为萌娘百科', host=host)
-                elif driver == '萌娘百科':
-                    driver = '搜狗百科'
-                    messagesend.sendmessage_txt(group=g, session=session, message='当前引擎切换为搜狗百科', host=host)
-                    web.get('https://baike.sogou.com/')
-            
             else:
-                messagesend.sendmessage_txt(group=g, session=session, message='只有管理员可以切换引擎', host=host)
-
-
-
-
-            
-    except:
+                sender.send_str(group=group, session=SESSION, message='只有管理员可以切换引擎', host=HOST)
+    except Exception:
         continue
-        
-        
-    
-    time.sleep(1)
-    
-    
 
-    
+    time.sleep(1)
